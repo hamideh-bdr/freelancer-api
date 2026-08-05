7f873cf72423c80366ec6c6872d4dd4433a47a05
Mahroo <226082914+hamideh-bdr@users.noreply.github.com>
2026-08-06 00:26:46 +0330

    feat: update isAdmin middleware

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const authMiddleware = require('../../middlewares/authMiddleware')
-const isAdminMiddleware = require('../../middlewares/isAdmin')
 const authController = require('../../controllers/v1/auth')
 const upload = require('../../middlewares/multer')
 
