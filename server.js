1d032bb6b9544dd4baa528b284c037a7e1ea19d5
Mahroo <226082914+hamideh-bdr@users.noreply.github.com>
2026-08-13 00:42:55 +0330

    test: verify running production source

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./src/app')
 require('dotenv').config()
 const connectDB = require('./src/config/db')
 const port = process.env.PORT;
+console.log("PRODUCTION BUILD TEST 001 ");
 
 const startServer = async() => {
     await connectDB();
